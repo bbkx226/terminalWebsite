6606acc2038b451d60d1ae072381297f2b2d5180
Brandon <bbkx226@gmail.com>
2023-02-18 21:47:29 +0800

    Updated some contents

--- public/js/main.js
+++ public/js/main.js
@@ -200,7 +200,7 @@ function enterKey(e) {
     if (e.keyCode == 13) {
       commands.push(command.innerHTML);
       git = commands.length;
-      addLine("guest@bbkx.live:~$ " + command.innerHTML, "no-animation", 0);
+      addLine("guest@bbkxterminal.web.app:~$ " + command.innerHTML, "no-animation", 0);
       commander(command.innerHTML.toLowerCase());
       command.innerHTML = "";
       textarea.value = "";
